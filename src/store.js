60b7b451e710876a863d766fdfff2831c972663c
Max Saienko <msaienko@ancestry.com>
2018-03-31 21:54:13 -0700

    commenting sagas wiring out, as I'm not gonna use them for this test

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { browserHistory } from "react-router";
 import { syncHistoryWithStore, routerMiddleware } from "react-router-redux";
-import createSagaMiddleware from "redux-saga";
+//import createSagaMiddleware from "redux-saga";
 import freeze from "redux-freeze";
 import { reducers } from "./reducers/index";
-import { sagas } from "./sagas/index";
+//import { sagas } from "./sagas/index";
 
 // add the middlewares
 let middlewares = [];
@@ -13,8 +13,8 @@ let middlewares = [];
 middlewares.push(routerMiddleware(browserHistory));
 
 // add the saga middleware
-const sagaMiddleware = createSagaMiddleware();
-middlewares.push(sagaMiddleware);
+// const sagaMiddleware = createSagaMiddleware();
+// middlewares.push(sagaMiddleware);
 
 // add the freeze dev middleware
 if (process.env.NODE_ENV !== 'production') {
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV !== 'production' && window.devToolsExtension) {
 // create the store
 const store = createStore(reducers, middleware);
 const history = syncHistoryWithStore(browserHistory, store);
-sagaMiddleware.run(sagas);
+//sagaMiddleware.run(sagas);
 
 // export
 export { store, history };
