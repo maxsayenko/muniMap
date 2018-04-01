21949d56a66a058f0fdb72c37ab0145ce4ce7422
Max Saienko <msaienko@ancestry.com>
2018-04-01 14:17:09 -0700

    initial map implementation (freeways)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,65 @@
-import React from "react";
+import React from 'react';
+import _ from 'lodash';
+import { Map, LineGroup } from 'react-d3-map';
 
-// Home page component
 export default class Home extends React.Component {
-  // render
-  render() {
-    return (
-      <div className="page-home">
-        <h4>Hello world!</h4>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.mapWidth = 2000;
+        this.mapHeight = 1800;
+        this.mapScale = 1 << 22;
+        this.mapScaleExtent = [1 << 20, 1 << 24];
+        this.mapCenter = [-122.42, 37.76];
+        this.state = {
+            freewaysData: null
+        };
+    }
+
+    componentDidMount() {
+        console.log('Mount');
+        fetch('/assets/sfmaps/freeways.json')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    freewaysData: data
+                });
+            });
+    }
+
+    render() {
+        console.log('Render');
+        console.log(this.state.freewaysData);
+
+        if(!this.state.freewaysData) {
+            return(
+                <div>Loading</div>
+            );
+        }
+
+
+
+        return (
+            <Map
+                width= {this.mapWidth}
+                height= {this.mapHeight}
+                scale= {this.mapScale}
+                scaleExtent = {this.mapScaleExtent}
+                center= {this.mapCenter}
+              >
+                  <g>
+                        <LineGroup
+                            key= {"line-test"}
+                            data= {this.state.freewaysData}
+                            //popupContent= {popupContent}
+                            // onClick= {onLineClick}
+                            // onCloseClick= {onLineCloseClick}
+                            // onMouseOver= {onLineMouseOver}
+                            // onMouseOut= {onLineMouseOut}
+                            meshClass= {"freeways"}
+                        />
+                  </g>
+              </Map>
+        )
+    }
 }
